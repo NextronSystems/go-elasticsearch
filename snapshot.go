a208e6dee01b473d306bc6316de41bd584473271
Marcel Gebhardt <marcel.gebhardt@bsk-consulting.de>
2018-06-14 12:57:53 +0200

    bugfix in snapshot

--- snapshot.go
+++ snapshot.go
@@ -25,6 +25,6 @@ func (c *Client) AddRepository(name string, location string) error {
 // AddSnapshot adds a new snapshot in a specified repository. Ensure that
 // the repository exists before calling this function.
 func (c *Client) AddSnapshot(repositoryName string, snapshotName string) error {
-	_, err := c.put(fmt.Sprintf("/_snapshot/%s/%s?wait_for_completion=true", repositoryName, snapshotName), nil)
+	_, err := c.put(fmt.Sprintf("_snapshot/%s/%s?wait_for_completion=true", repositoryName, snapshotName), nil)
 	return err
 }
